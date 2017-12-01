4dc025dbce164ec606c0cb35d8cbcc009b28d3c6
cavimora <cavimora@gmail.com>
2017-12-01 16:36:55 -0600

    no message

--- CreditoCobro/CreditoCobro.AplicacionBanco/FrmOperations.Designer.cs
+++ CreditoCobro/CreditoCobro.AplicacionBanco/FrmOperations.Designer.cs
@@ -29,21 +29,235 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmOperations));
+            this.dtvCreditos = new System.Windows.Forms.DataGridView();
+            this.Monto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tasa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Plazo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtApellido1 = new MetroFramework.Controls.MetroTextBox();
+            this.txtNombre = new MetroFramework.Controls.MetroTextBox();
+            this.txtCedula = new MetroFramework.Controls.MetroTextBox();
+            this.lblApellido1 = new MetroFramework.Controls.MetroLabel();
+            this.lblNombre = new MetroFramework.Controls.MetroLabel();
+            this.lblCedula = new MetroFramework.Controls.MetroLabel();
+            this.btnEliminar = new MetroFramework.Controls.MetroButton();
+            this.btnNuevo = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvCreditos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dtvCreditos
+            // 
+            this.dtvCreditos.AllowUserToResizeColumns = false;
+            this.dtvCreditos.AllowUserToResizeRows = false;
+            this.dtvCreditos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtvCreditos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvCreditos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Monto,
+            this.Tasa,
+            this.Plazo});
+            this.dtvCreditos.Location = new System.Drawing.Point(23, 207);
+            this.dtvCreditos.MultiSelect = false;
+            this.dtvCreditos.Name = "dtvCreditos";
+            this.dtvCreditos.ReadOnly = true;
+            this.dtvCreditos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtvCreditos.Size = new System.Drawing.Size(343, 394);
+            this.dtvCreditos.TabIndex = 15;
+            // 
+            // Monto
+            // 
+            this.Monto.DataPropertyName = "Monto";
+            this.Monto.HeaderText = "Monto";
+            this.Monto.Name = "Monto";
+            this.Monto.ReadOnly = true;
+            // 
+            // Tasa
+            // 
+            this.Tasa.DataPropertyName = "Tasa";
+            this.Tasa.HeaderText = "Tasa";
+            this.Tasa.Name = "Tasa";
+            this.Tasa.ReadOnly = true;
+            // 
+            // Plazo
+            // 
+            this.Plazo.DataPropertyName = "Plazo";
+            this.Plazo.HeaderText = "Plazo";
+            this.Plazo.Name = "Plazo";
+            this.Plazo.ReadOnly = true;
+            // 
+            // txtApellido1
+            // 
+            // 
+            // 
+            // 
+            this.txtApellido1.CustomButton.Image = null;
+            this.txtApellido1.CustomButton.Location = new System.Drawing.Point(229, 1);
+            this.txtApellido1.CustomButton.Name = "";
+            this.txtApellido1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtApellido1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtApellido1.CustomButton.TabIndex = 1;
+            this.txtApellido1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtApellido1.CustomButton.UseSelectable = true;
+            this.txtApellido1.CustomButton.Visible = false;
+            this.txtApellido1.Enabled = false;
+            this.txtApellido1.Lines = new string[0];
+            this.txtApellido1.Location = new System.Drawing.Point(115, 135);
+            this.txtApellido1.MaxLength = 32767;
+            this.txtApellido1.Name = "txtApellido1";
+            this.txtApellido1.PasswordChar = '\0';
+            this.txtApellido1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtApellido1.SelectedText = "";
+            this.txtApellido1.SelectionLength = 0;
+            this.txtApellido1.SelectionStart = 0;
+            this.txtApellido1.ShortcutsEnabled = true;
+            this.txtApellido1.Size = new System.Drawing.Size(251, 23);
+            this.txtApellido1.TabIndex = 33;
+            this.txtApellido1.UseSelectable = true;
+            this.txtApellido1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtApellido1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtNombre
+            // 
+            // 
+            // 
+            // 
+            this.txtNombre.CustomButton.Image = null;
+            this.txtNombre.CustomButton.Location = new System.Drawing.Point(229, 1);
+            this.txtNombre.CustomButton.Name = "";
+            this.txtNombre.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtNombre.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtNombre.CustomButton.TabIndex = 1;
+            this.txtNombre.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtNombre.CustomButton.UseSelectable = true;
+            this.txtNombre.CustomButton.Visible = false;
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Lines = new string[0];
+            this.txtNombre.Location = new System.Drawing.Point(115, 95);
+            this.txtNombre.MaxLength = 32767;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.PasswordChar = '\0';
+            this.txtNombre.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtNombre.SelectedText = "";
+            this.txtNombre.SelectionLength = 0;
+            this.txtNombre.SelectionStart = 0;
+            this.txtNombre.ShortcutsEnabled = true;
+            this.txtNombre.Size = new System.Drawing.Size(251, 23);
+            this.txtNombre.TabIndex = 32;
+            this.txtNombre.UseSelectable = true;
+            this.txtNombre.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtNombre.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtCedula
+            // 
+            // 
+            // 
+            // 
+            this.txtCedula.CustomButton.Image = null;
+            this.txtCedula.CustomButton.Location = new System.Drawing.Point(229, 1);
+            this.txtCedula.CustomButton.Name = "";
+            this.txtCedula.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtCedula.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtCedula.CustomButton.TabIndex = 1;
+            this.txtCedula.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtCedula.CustomButton.UseSelectable = true;
+            this.txtCedula.CustomButton.Visible = false;
+            this.txtCedula.Enabled = false;
+            this.txtCedula.Lines = new string[0];
+            this.txtCedula.Location = new System.Drawing.Point(115, 56);
+            this.txtCedula.MaxLength = 32767;
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.PasswordChar = '\0';
+            this.txtCedula.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtCedula.SelectedText = "";
+            this.txtCedula.SelectionLength = 0;
+            this.txtCedula.SelectionStart = 0;
+            this.txtCedula.ShortcutsEnabled = true;
+            this.txtCedula.Size = new System.Drawing.Size(251, 23);
+            this.txtCedula.TabIndex = 31;
+            this.txtCedula.UseSelectable = true;
+            this.txtCedula.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtCedula.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblApellido1
+            // 
+            this.lblApellido1.AutoSize = true;
+            this.lblApellido1.Location = new System.Drawing.Point(23, 139);
+            this.lblApellido1.Name = "lblApellido1";
+            this.lblApellido1.Size = new System.Drawing.Size(67, 19);
+            this.lblApellido1.TabIndex = 30;
+            this.lblApellido1.Text = "Apellido 1";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(23, 99);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 19);
+            this.lblNombre.TabIndex = 29;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Location = new System.Drawing.Point(23, 60);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(50, 19);
+            this.lblCedula.TabIndex = 28;
+            this.lblCedula.Text = "Cédula";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(247, 164);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(119, 37);
+            this.btnEliminar.TabIndex = 35;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseSelectable = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(23, 164);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(119, 37);
+            this.btnNuevo.TabIndex = 34;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseSelectable = true;
             // 
             // FrmOperations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(990, 648);
+            this.ClientSize = new System.Drawing.Size(395, 622);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.txtApellido1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.lblApellido1);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.dtvCreditos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmOperations";
             this.Text = "Operaciónes";
             this.Load += new System.EventHandler(this.FrmOperations_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtvCreditos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dtvCreditos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Monto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tasa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Plazo;
+        private MetroFramework.Controls.MetroTextBox txtApellido1;
+        private MetroFramework.Controls.MetroTextBox txtNombre;
+        private MetroFramework.Controls.MetroTextBox txtCedula;
+        private MetroFramework.Controls.MetroLabel lblApellido1;
+        private MetroFramework.Controls.MetroLabel lblNombre;
+        private MetroFramework.Controls.MetroLabel lblCedula;
+        private MetroFramework.Controls.MetroButton btnEliminar;
+        private MetroFramework.Controls.MetroButton btnNuevo;
     }
 }
